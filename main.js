47f1d54d76d59f87d39ec228cfcbd5dd91a5e0fd
Junior Colares <98316365+JuniorColares@users.noreply.github.com>
2022-04-19 14:26:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,159 @@
+function mostrar1() {
+    document.getElementById('solucao1').style.display = "block";
+}
+
+function ocultar1() {
+    document.getElementById('solucao1').style.display = "none";
+}
+
+function mostrar2() {
+    document.getElementById('solucao2').style.display = "block";
+}
+
+function ocultar2() {
+    document.getElementById('solucao2').style.display = "none";
+}
+
+function mostrar3() {
+    document.getElementById('solucao3').style.display = "block";
+}
+
+function ocultar3() {
+    document.getElementById('solucao3').style.display = "none";
+}
+
+function mostrar4() {
+    document.getElementById('solucao4').style.display = "block";
+}
+
+function ocultar4() {
+    document.getElementById('solucao4').style.display = "none";
+}
+
+function mostrar5() {
+    document.getElementById('solucao5').style.display = "block";
+}
+
+function ocultar5() {
+    document.getElementById('solucao5').style.display = "none";
+}
+
+function mostrar6() {
+    document.getElementById('solucao6').style.display = "block";
+}
+
+function ocultar6() {
+    document.getElementById('solucao6').style.display = "none";
+}
+
+function mostrar7() {
+    document.getElementById('solucao7').style.display = "block";
+}
+
+function ocultar7() {
+    document.getElementById('solucao7').style.display = "none";
+}
+
+function mostrar8() {
+    document.getElementById('solucao8').style.display = "block";
+}
+
+function ocultar8() {
+    document.getElementById('solucao8').style.display = "none";
+}
+
+function mostrar9() {
+    document.getElementById('solucao9').style.display = "block";
+}
+
+function ocultar9() {
+    document.getElementById('solucao9').style.display = "none";
+}
+
+function mostrar10() {
+    document.getElementById('solucao10').style.display = "block";
+}
+
+function ocultar10() {
+    document.getElementById('solucao10').style.display = "none";
+}
+
+function mostrar11() {
+    document.getElementById('solucao11').style.display = "block";
+}
+
+function ocultar11() {
+    document.getElementById('solucao11').style.display = "none";
+}
+
+function mostrar12() {
+    document.getElementById('solucao12').style.display = "block";
+}
+
+function ocultar12() {
+    document.getElementById('solucao12').style.display = "none";
+}
+
+function mostrar13() {
+    document.getElementById('solucao13').style.display = "block";
+}
+
+function ocultar13() {
+    document.getElementById('solucao13').style.display = "none";
+}
+
+function mostrar14() {
+    document.getElementById('solucao14').style.display = "block";
+}
+
+function ocultar14() {
+    document.getElementById('solucao14').style.display = "none";
+}
+
+function mostrar15() {
+    document.getElementById('solucao15').style.display = "block";
+}
+
+function ocultar15() {
+    document.getElementById('solucao15').style.display = "none";
+}
+
+function mostrar16() {
+    document.getElementById('solucao16').style.display = "block";
+}
+
+function ocultar16() {
+    document.getElementById('solucao16').style.display = "none";
+}
+
+function mostrar17() {
+    document.getElementById('solucao17').style.display = "block";
+}
+
+function ocultar17() {
+    document.getElementById('solucao17').style.display = "none";
+}
+
+function mostrar18() {
+    document.getElementById('solucao18').style.display = "block";
+}
+
+function ocultar18() {
+    document.getElementById('solucao18').style.display = "none";
+}
+
+function mostrar19() {
+    document.getElementById('solucao19').style.display = "block";
+}
+
+function ocultar19() {
+    document.getElementById('solucao19').style.display = "none";
+}
+
+function mostrar20() {
+    document.getElementById('solucao20').style.display = "block";
+}
+
+function ocultar20() {
+    document.getElementById('solucao20').style.display = "none";
+}
